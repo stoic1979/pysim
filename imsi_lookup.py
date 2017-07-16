904c616fa4b30a7480301f46b9ab7aa2d5f198ec
Navjot Singh <weavebytes@gmail.com>
2017-07-16 16:27:43 +0530

    imsi lookup util

---
+++ imsi_lookup.py
@@ -0,0 +1,13 @@
+#
+# Script for looking/determining MCC, MNC, Carrier/Provider
+# from a IMSI code
+#
+
+
+# FIXME
+# add code to look up IMSI
+
+
+
+if __name__ == '__main__':
+    # quick internal tests
